69fe2c456558411b12f70d4f6303bc4536c65b6e
Flavia0501 <flavia41670@fiec.edu.br>
2025-09-29 17:15:17 -0300

    Código adaptado assim como nas aulas do fillipe

---
+++ src/main/java/com/PetMatch/PetMatchBackEnd/features/user/admin/controllers/AdminController.java
@@ -0,0 +1,16 @@
+package com.PetMatch.PetMatchBackEnd.features.user.admin.controllers;
+
+import com.PetMatch.PetMatchBackEnd.features.user.models.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/admin")
+public class AdminController {
+
+    public void testRequest(Authentication authentication) {
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+        System.out.println(usuario);
+    }
+}
